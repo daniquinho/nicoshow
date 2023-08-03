48dcd3f607915512677155dd9c14b58844382c0b
daniquinho <141362717+daniquinho@users.noreply.github.com>
2023-08-03 16:42:45 -0300

    cheerio

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const axios = require('axios')
+const cheerio = require('cheerio')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/get', async (req, res) => {
+  var f = await axios.get('https://google.com');
+  const $ = cheerio.load(f.data);
+  var t = $('span').text();
+  res.send(t);
 })
 
 app.listen(port, () => {
