07368d1462ecee08f8d497e2d7a8cd0e8b79ef8d
daniquinho <141362717+daniquinho@users.noreply.github.com>
2023-08-05 14:52:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const cheerio = require('cheerio')
 
 app.use(express.json());
 
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  return array;
+}
+
+
+
 const getXvideos = async (url) => {
   var x = await axios.get(url);
   const $ = cheerio.load(x.data)
@@ -50,6 +60,55 @@ async function getVideoUrl(video) {
 }
 
 
+const getBoyfriendVideos = async (search='', page=1) => {
+  var url = 'https://www.boyfriendtv.com/';
+  if(search){
+  	url+= 'search/' + search.replace(/ /g, '+') + '/?page=' + page;
+  }else{
+  	url += 'videos/best-recent/' + page;
+  }
+  console.log(url)
+  var x = await axios.get(url);
+  const $ = cheerio.load(x.data)
+  var videos = [];
+  $('ul.block.ul-no-dots > li.js-pop.thumb-item.videospot').each((i, el) => {
+  	var dur = '';
+  	$(el).find('.fs11.viddata.flr.time-label-wrapper').children().each((j, e) => {
+  		if(!$(e).attr('class'))
+  			dur = $(e).text()
+  	})
+  	var link = 'https://www.boyfriendtv.com/videos/' + $(el).find('.thumb.vidItem').attr('data-video-id');
+  	videos.push({
+  		title: $(el).find('p.titlevideospot > a').attr('title'),
+  		link: link,
+  		img: $(el).find('.thumb-inner-wrapper > a > img').attr('src') ? $(el).find('.thumb-inner-wrapper > a > img').attr('src'): null,
+  		quality: $(el).find('span.text-active.bold').text() || "360p",
+  		duration: dur.trim() || '00:00',
+  		download: '/downloadBoyfriend?url=' + encodeURIComponent(link)
+  	})
+  })
+  return videos;
+}
+
+async function downloadBoyfriend(url) {
+	var data = await axios.get(url);
+	const $ = cheerio.load(data.data);
+
+	var video = $('video source').attr('src');
+	return video;
+} 
+
+async function randomVideos(search, page) {
+	var x = await xvideos('gay', search, page);
+	var b = await getBoyfriendVideos(search, page);
+
+	var tudo = x.concat(b);
+	tudo = shuffleArray(tudo)
+
+	return tudo;
+}
+
+
 app.get('/', async (req, res) => {
 	var data = await xvideos(req.query.cat, req.query.k, req.query.page);
 	res.json(data);
@@ -72,6 +131,25 @@ app.get('/high', async (req, res) => {
 	var url = decodeURIComponent(req.query.url);
 	var data = await getVideoUrl(url);
 	res.redirect(data.high);
+})
+
+app.get('/boyfriend', async (req, res) => {
+	var data = await getBoyfriendVideos()
+	res.json(data);
+})
+
+app.get('/downloadBoyfriend', async (req, res) => {
+	if(!req.query.url)
+		res.sendStatus(404);
+
+	var url = decodeURIComponent(req.query.url);
+	var data = await downloadBoyfriend(url);
+	res.redirect(data);
+})
+
+app.get('/random', async (req, res) => {
+	var data = await randomVideos(req.query.k, req.query.page);
+	res.json(data);
 })
 
 app.listen(port, () => {
